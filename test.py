b9445fa260c6e3b28d7c8a77db2c9a0d872b69eb
Dunno358 <89750169+Dunno358@users.noreply.github.com>
2021-10-06 22:27:28 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Write(pygame.sprite.Sprite):
-    def __init__(self,size,text,color,center):
-        pygame.sprite.Sprite.__init__(self)
-        self.textObject = pygame.font.Font(r"C:\SimpleCipher\Data\Fonts\TrajanPro\TrajanPro-Regular.ttf",size)
-        self.writeText = self.textObject.render(text,True,color)
-        self.textRect = self.writeText.get_rect()
-        self.textRect.center = center
