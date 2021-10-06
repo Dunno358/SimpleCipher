e88feee60ccead6389dea6c08b8f07c9f54f9476
Dunno358 <89750169+Dunno358@users.noreply.github.com>
2021-10-06 22:26:32 +0200

    Delete setup3.py

--- setup3.py
+++
@@ -1,9 +0,0 @@
-#setup for converting to exe by py2exe - old
-import pygame
-pygame.init()
-from distutils.core import setup
-import py2exe
-import os
-
-setup(console=[r"C:\Users\Mariusz\Desktop\Projects\Python\SimpleCipher\Data\SimpleCipher-Decipher.py"])
-os.system(r'pause')
